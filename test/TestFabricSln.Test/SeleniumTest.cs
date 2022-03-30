786aad92e347f73b14fd893c8038d91d2a9cab71
vernon.naidoo@altron.com <Admin@PRVRNMES01>
2022-03-30 22:13:34 +0200

    added selenium tests

---
+++ test/TestFabricSln.Test/SeleniumTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace TestFabricSln.Test
+{
+    class SeleniumTest
+    {
+        [Test]
+        [Category("UITests")]
+        public void VisitMicrosoft_CheckWindowsMenu()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.microsoft.com/");
+            Thread.Sleep(10000);
+            string Windows_Text = driver.FindElement(By.Id("shellmenu_2")).Text;
+            Assert.AreEqual("Windows", Windows_Text);
+            driver.Quit();
+        }
+    }
+}
