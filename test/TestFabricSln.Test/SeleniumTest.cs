50d2d1893697c1338a5b0633872cd80f22d99aff
vernon.naidoo@altron.com <Admin@PRVRNMES01>
2022-03-31 11:47:40 +0200

    automated getting chromedriver using webdrivermanager using explicit waits

--- test/TestFabricSln.Test/SeleniumTest.cs
+++ test/TestFabricSln.Test/SeleniumTest.cs
@@ -1,22 +1,56 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
-using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+using System;
 
 namespace TestFabricSln.Test
 {
-    class SeleniumTest
+    public class SeleniumTest
     {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+
+            //DesiredCapabilities capabilities = new DesiredCapabilities();
+            //capabilities.setCapability("something", true);
+
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("--headless");
+            option.AddArgument("--disable-gpu");
+            option.AddArguments("--window-size=1280,800");
+            option.AddArguments("--allow-insecure-localhost");
+
+            //specifically this line here :)
+            option.AddAdditionalCapability("acceptInsecureCerts", true, true);
+
+            //options.merge(capabilities);
+
+            _webDriver = new ChromeDriver(option);
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);            
+        }
+
         [Test]
         [Category("UITests")]
         public void VisitMicrosoft_CheckWindowsMenu()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.microsoft.com/");
-            Thread.Sleep(10000);
-            string Windows_Text = driver.FindElement(By.Id("shellmenu_2")).Text;
-            Assert.AreEqual("Windows", Windows_Text);
-            driver.Quit();
+            _webDriver.Navigate().GoToUrl("https://www.microsoft.com/");
+            WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+            IWebElement winMenu = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(_webDriver.FindElement(By.Id("shellmenu_2"))));
+
+            Assert.AreEqual("Windows", winMenu.Text);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
         }
     }
 }
